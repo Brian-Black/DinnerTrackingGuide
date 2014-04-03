d3c1744ba9fc808b2a97d9cbaa9c81d1816f02a4
Brian-Black <Brian.Blackph@gmail.com>
2014-04-03 13:40:21 -0600

    added all the HTML as links

--- __init__.py
+++ __init__.py
@@ -1,12 +1,12 @@
 from flask import Flask, url_for, request
 from flask import render_template
-from flask.ext.mongoengine import MongoEngine
+#from flask.ext.mongoengine import MongoEngine
 
 app = Flask(__name__)
-app.config["MONGODB_SETTINGS"] = {'DB': "Recipies"}
-app.config["SECRET_KEY"] = "KeepThisS3cr3t"
+#app.config["MONGODB_SETTINGS"] = {'DB': "Recipies"}
+#app.config["SECRET_KEY"] = "KeepThisS3cr3t"
 
-db = MongoEngine(app)
+#db = MongoEngine(app)
 
 @app.route('/')
 def index():
@@ -17,9 +17,21 @@ def index():
 def hello(name=None):
 	return render_template('hello.html', name=name)
 
-@app.route('/add_recipe')
+@app.route('/add_recipe/')
 def addRecipe():
 	return render_template('design_edit_recipe.html')
+
+@app.route('/recipe/')
+def viewRecipe():
+	return render_template('design_recipe.html')
+
+@app.route('/shopping/')
+def shopping():
+	return render_template('design_shopping_list.html')
+
+@app.route('/my_recipes/')
+def myRecipe():
+	return render_template('design_myRecipes.html')
 
 if __name__ == '__main__':
 	app.run()
